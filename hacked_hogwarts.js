c86edabd3e0f838d20ddc494ab0b27ed995f6265
Lasse <lass948m@stud.kea.dk>
2021-09-25 09:03:20 +0200

    fixet popup samt indsat billeder

--- hacked_hogwarts.js
+++ hacked_hogwarts.js
@@ -2,10 +2,13 @@
 
 //Henter jsondata 
 async function getData() {
-    const link = "https://petlatkea.dk/2021/hogwarts/students.json"
-    const response = await fetch(link)
-    const jdata = await response.json()
-    return jdata
+    const studentLink = "https://petlatkea.dk/2021/hogwarts/students.json"
+    const studentResponse = await fetch(studentLink)
+    const studentData = await studentResponse.json()
+    const familyLink = 'https://petlatkea.dk/2021/hogwarts/families.json'
+    const familyResponse = await fetch(familyLink)
+    const familyData = await familyResponse.json()
+    return { studentData, familyData }
 }
 
 
@@ -22,9 +25,9 @@ function capitalize(name) {
 }
 
 
-function cleanData(jdata) {
+function cleanData(studentData, familyMap) {
     const clean = []
-    jdata.forEach(student => {
+    studentData.forEach(student => {
         let fullname = student.fullname.trim()
         const parts = fullname.split(" ")
         const firstname = capitalize(parts[0])
@@ -36,27 +39,27 @@ function cleanData(jdata) {
         if (parts.length === 3 && parts[1].includes(`"`)) {
             nickname = capitalize(parts[1].substring(1, parts[1].lastIndexOf('"')))
             lastname = capitalize(parts[2])
+            fullname = `${firstname} "${nickname}" ${lastname}`
         } else if (parts.length === 2 && parts[1].includes(`"`)) {
             nickname = capitalize(parts[1].substring(1, parts[1].lastIndexOf('"')))
+            fullname = `${firstname}`
         } else if (parts.length === 3) {
             middlename = capitalize(parts[1])
             lastname = capitalize(parts[2])
+            fullname = `${firstname} "${middlename}" ${lastname}`
         } else if (parts.length == 2) {
             lastname = capitalize(parts[1])
-        }
-
-        if (middlename === null) {
-            fullname = capitalize(`${firstname}`) + " " + capitalize(`${lastname}`);
+            fullname = `${firstname} ${lastname}`
         } else {
-            fullname = capitalize(`${firstname}`) + " " + capitalize(`${middlename}`) + " " + capitalize(`${lastname}`);
+            fullname = firstname
         }
-        if (lastname === null) {
-            fullname = capitalize(`${firstname}`)
-        }
-        let gender = capitalize(student.gender.trim());
-        let house = capitalize(student.house.trim());
-        // fullname = capitalize(`${firstname}`) + " " + capitalize(`${middlename}`) + " " + capitalize(`${lastname}`);
 
+        const gender = capitalize(student.gender.trim());
+        const house = capitalize(student.house.trim());
+
+        const bloodstatus = familyMap[lastname] || 'Unknown'
+
+        const imagePath = lastname ? `${lastname.toLowerCase()}_${firstname[0].toLowerCase()}` : null
 
         const studentObject = {
             fullname,
@@ -65,32 +68,91 @@ function cleanData(jdata) {
             nickname,
             lastname,
             gender,
-            house
+            house,
+            bloodstatus,
+            imagePath
         }
         clean.push(studentObject)
     })
-    show(clean)
 
     return clean
 }
-const container = document.querySelector("#container");
-const template = document.querySelector("template");
 
-function show (student) {
-    student.forEach(student => {
-        let clone = template.cloneNode(true).content;
-        clone.querySelector("#students").textContent = student.fullname
-        container.appendChild(clone)
+function updateView () {
+    const template = document.querySelector("#student_template");
+    const studentsContainer = document.querySelector("#students")
+
+    studentsContainer.innerHTML = null
+
+    sortedStudents = [...students].sort((a, b) => a[sortKey] > b[sortKey] ? sortOrder : sortOrder * -1)
+
+    sortedStudents.forEach((student, index) => {
+        const clone = template.cloneNode(true).content;
+        clone.querySelector(".student").setAttribute('data-student-index', index)
+        clone.querySelector(".student_firstname").textContent = student.firstname
+        clone.querySelector(".student_lastname").textContent = student.lastname || '-'
+        clone.querySelector(".student_house").textContent = student.house
+        studentsContainer.appendChild(clone)
     });
+
+    updateCount()
 }
 
+function updateCount () {
+    document.querySelector('#count').textContent = students.length
+}
 
-async function main () {
+function showPopup (studentElement) {
+    const studentIndex = studentElement.getAttribute('data-student-index')
+    const student = sortedStudents[studentIndex]
+
+    const popup = document.querySelector("#popup")
+    popup.classList.add("show")
+
+    popup.querySelector("#popup_fullname").textContent = student.fullname
+    popup.querySelector("#popup_gender").textContent = student.gender
+    popup.querySelector("#popup_house").textContent = student.house
+    popup.querySelector("#popup_bloodstatus").textContent = student.bloodstatus
+    if (student.imagePath) popup.querySelector("#popup_image").setAttribute('src', `images/${student.imagePath}.png`)
+
+    const colorMap = {
+        'Gryffindor': '#740001',
+        'Slytherin': '#1A472A',
+        'Ravenclaw': '#0E1A40',
+        'Hufflepuff': '#FFDB00'
+
+    }
+
+    popup.style.background = colorMap[student.house]
+
+}
+
+function closePopup () {
+    document.querySelector("#popup").classList.remove("show")
+}
+
+function sort (key) {
+    if (sortKey === key) sortOrder *= -1
+    else {
+        sortKey = key
+        sortOrder = 1
+    }
+    updateView()
+}
+
+let students = null
+let sortedStudents = null
+let sortKey = 'firstname'
+let sortOrder = 1
+
+async function initialize () {
     const data = await getData()
-    const clean = cleanData(data)
-    console.log('clean', clean)
+    const familyMap = {}
+    data.familyData.half.forEach(lastname => familyMap[lastname] = 'Half')
+    data.familyData.pure.forEach(lastname => familyMap[lastname] = 'Pure')
+    students = cleanData(data.studentData, familyMap)
+    updateView()
 }
 
-main()
-
+initialize()
 
